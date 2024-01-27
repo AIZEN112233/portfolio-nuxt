aca7940a1cf61cfc0ada4d54902c45adc491b4e6
Aizen <acristyano22@gmail.com>
2024-01-27 19:37:40 +0100

    init: add sections

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+	<main>
+		<SectionHome />
+		<SectionAbout />
+		<SectionSkills />
+		<SectionProjects />
+		<SectionContact />
+	</main>
+</template>
