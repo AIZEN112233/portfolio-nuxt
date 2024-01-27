0981a4282170609d239964845d6a7c3fcb52d562
Aizen <acristyano22@gmail.com>
2024-01-27 23:56:02 +0100

    feat: add hover effect (change to white when hover)

--- components/icon/Node.vue
+++ components/icon/Node.vue
@@ -1,100 +1,91 @@
 <template>
 	<svg
+		@mouseenter="hover = true"
+		@mouseleave="hover = false"
 		xmlns="http://www.w3.org/2000/svg"
 		width="50"
 		height="50"
-		viewBox="0 0 256 292">
+		viewBox="0 0 128 128">
+		<path
+			:fill="!hover ? '#58A544' : '#fff'"
+			class="duration-400"
+			d="M66.958.825a6.07 6.07 0 0 0-6.035 0L11.103 29.76c-1.895 1.072-2.96 3.095-2.96 5.24v57.988c0 2.143 1.183 4.167 2.958 5.24l49.82 28.934a6.07 6.07 0 0 0 6.036 0l49.82-28.935c1.894-1.072 2.958-3.096 2.958-5.24V35c0-2.144-1.183-4.167-2.958-5.24z"></path>
+		<path
+			:fill="!hover ? '#4D9742' : '#fff'"
+			class="duration-400"
+			d="M116.897 29.76L66.841.825A8.161 8.161 0 0 0 65.302.23L9.21 96.798a6.251 6.251 0 0 0 1.657 1.43l50.057 28.934c1.42.833 3.076 1.072 4.615.595l52.66-96.925a3.702 3.702 0 0 0-1.302-1.072"></path>
+		<path
+			:fill="!hover ? '#58A544' : '#fff'"
+			class="duration-400"
+			d="M116.898 98.225c1.42-.833 2.485-2.262 2.958-3.81L65.066.108c-1.42-.238-2.959-.119-4.26.715L11.104 29.639l53.606 98.355c.71-.12 1.54-.358 2.25-.715z"></path>
 		<defs>
 			<linearGradient
-				id="logosNodejsIconAlt0"
-				x1="68.188%"
-				x2="27.823%"
-				y1="17.487%"
-				y2="89.755%">
+				id="deviconNodejs0"
+				x1="34.513"
+				x2="27.157"
+				y1="15.535"
+				y2="30.448"
+				gradientTransform="translate(-129.242 -73.715)scale(6.18523)"
+				gradientUnits="userSpaceOnUse">
+				<stop stopColor="#3F873F"></stop>
 				<stop
-					offset="0%"
-					stopColor="#41873F"></stop>
+					offset=".33"
+					stopColor="#3F8B3D"></stop>
 				<stop
-					offset="32.88%"
-					stopColor="#418B3D"></stop>
+					offset=".637"
+					stopColor="#3E9638"></stop>
 				<stop
-					offset="63.52%"
-					stopColor="#419637"></stop>
+					offset=".934"
+					stopColor="#3DA92E"></stop>
 				<stop
-					offset="93.19%"
-					stopColor="#3FA92D"></stop>
-				<stop
-					offset="100%"
-					stopColor="#3FAE2A"></stop>
+					offset="1"
+					stopColor="#3DAE2B"></stop>
 			</linearGradient>
 			<linearGradient
-				id="logosNodejsIconAlt1"
-				x1="43.277%"
-				x2="159.245%"
-				y1="55.169%"
-				y2="-18.306%">
+				id="deviconNodejs1"
+				x1="30.009"
+				x2="50.533"
+				y1="23.359"
+				y2="8.288"
+				gradientTransform="translate(-129.242 -73.715)scale(6.18523)"
+				gradientUnits="userSpaceOnUse">
 				<stop
-					offset="13.76%"
-					stopColor="#41873F"></stop>
+					offset=".138"
+					stopColor="#3F873F"></stop>
 				<stop
-					offset="40.32%"
-					stopColor="#54A044"></stop>
+					offset=".402"
+					stopColor="#52A044"></stop>
 				<stop
-					offset="71.36%"
-					stopColor="#66B848"></stop>
+					offset=".713"
+					stopColor="#64B749"></stop>
 				<stop
-					offset="90.81%"
-					stopColor="#6CC04A"></stop>
+					offset=".908"
+					stopColor="#6ABF4B"></stop>
 			</linearGradient>
 			<linearGradient
-				id="logosNodejsIconAlt2"
-				x1="-4.389%"
-				x2="101.499%"
-				y1="49.997%"
-				y2="49.997%">
+				id="deviconNodejs2"
+				x1="21.917"
+				x2="40.555"
+				y1="22.261"
+				y2="22.261"
+				gradientTransform="translate(-129.242 -73.715)scale(6.18523)"
+				gradientUnits="userSpaceOnUse">
 				<stop
-					offset="9.192%"
-					stopColor="#6CC04A"></stop>
+					offset=".092"
+					stopColor="#6ABF4B"></stop>
 				<stop
-					offset="28.64%"
-					stopColor="#66B848"></stop>
+					offset=".287"
+					stopColor="#64B749"></stop>
 				<stop
-					offset="59.68%"
-					stopColor="#54A044"></stop>
+					offset=".598"
+					stopColor="#52A044"></stop>
 				<stop
-					offset="86.24%"
-					stopColor="#41873F"></stop>
+					offset=".862"
+					stopColor="#3F873F"></stop>
 			</linearGradient>
-			<path
-				id="logosNodejsIconAlt3"
-				d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944z"></path>
 		</defs>
-		<path
-			fill="url(#logosNodejsIconAlt0)"
-			d="M134.923 1.832c-4.344-2.443-9.502-2.443-13.846 0L6.787 67.801C2.443 70.244 0 74.859 0 79.745v132.208c0 4.887 2.715 9.502 6.787 11.945l114.29 65.968c4.344 2.444 9.502 2.444 13.846 0l114.29-65.968c4.344-2.443 6.787-7.058 6.787-11.945V79.745c0-4.886-2.715-9.501-6.787-11.944z"></path>
-		<mask
-			id="logosNodejsIconAlt4"
-			fill="#fff">
-			<use href="#logosNodejsIconAlt3"></use>
-		</mask>
-		<path
-			fill="url(#logosNodejsIconAlt1)"
-			d="M249.485 67.8L134.65 1.833c-1.086-.542-2.443-1.085-3.529-1.357L2.443 220.912c1.086 1.357 2.444 2.443 3.8 3.258l114.834 65.968c3.258 1.9 7.059 2.443 10.588 1.357L252.47 70.515c-.815-1.086-1.9-1.9-2.986-2.714"
-			mask="url(#logosNodejsIconAlt4)"></path>
-		<mask
-			id="logosNodejsIconAlt5"
-			fill="#fff">
-			<use href="#logosNodejsIconAlt3"></use>
-		</mask>
-		<path
-			fill="url(#logosNodejsIconAlt2)"
-			d="M249.756 223.898c3.258-1.9 5.701-5.158 6.787-8.687L130.579.204c-3.258-.543-6.787-.272-9.773 1.628L6.786 67.53l122.979 224.238c1.628-.272 3.529-.815 5.158-1.63z"
-			mask="url(#logosNodejsIconAlt5)"></path>
 	</svg>
 </template>
-
-<script lang="ts">
-	export default {
-		name: "LogosNodejsIconAlt",
-	};
+<script lang="ts" setup>
+	const hover = ref(false);
 </script>
