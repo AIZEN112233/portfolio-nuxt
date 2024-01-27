aeefc06763ddfbbbd27ae7ed09e6b5c2bec6a21e
Aizen <acristyano22@gmail.com>
2024-01-27 19:02:40 +0100

    feat: add navLinks to constants

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+	{
+		title: "Home",
+		path: "#home",
+	},
+	{
+		title: "Rate Us",
+		path: "#rate-us",
+	},
+	{
+		title: "Skills",
+		path: "#skills",
+	},
+	{
+		title: "About",
+		path: "#about",
+	},
+	{
+		title: "Projects",
+		path: "#projects",
+	},
+];
